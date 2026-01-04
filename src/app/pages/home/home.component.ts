ac5bf6d1d97f53cd7bc0676ec1e83adcfc652280
udzandy <sandeepani.udara@gmail.com>
2026-01-04 20:04:55 +0800

    Modified the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,34 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-amenities = [
-    {
-      name: 'BBQ',
-      price: '$20 / Day',
-      image: 'assets/images/bbq.jpg'
-      // image: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc'
-    },
-    {
-      name: 'Function Room',
-      price: '$30 / Day',
-      image: 'assets/images/function-room.jpg'
-      // image: 'https://images.unsplash.com/photo-1505693416388-ac5ce068fe85'
-    },
-    {
-      name: 'Tennis Court',
-      price: '$10 / Day',
-      image: 'assets/images/tennis-court.jpg'
-    },
-    // {
-    //   name: 'Gym',
-    //   price: 'No Fee',
-    //   image: 'https://images.unsplash.com/photo-1558611848-73f7eb4001a1'
-    // },
-    // {
-    //   name: 'Rooftop Patio',
-    //   price: 'No Fee',
-    //   image: 'https://images.unsplash.com/photo-1502673530728-f79b4cab31b1'
-    // }
+  amenities = [
+    { name: 'BBQ', price: '$20 / Day', image: 'assets/images/bbq.jpg', route: 'bbq' },
+    { name: 'Function Room', price: '$30 / Day', image: 'assets/images/function-room.jpg', route: 'function-room' },
+    { name: 'Tennis Court', price: '$10 / Day', image: 'assets/images/tennis-court.jpg', route: 'tennis-court' }
   ];
 
+  constructor(private router: Router) {}
+
+  goToBooking(route: string) {
+    this.router.navigate(['/booking', route]);
+  }
 }
+
